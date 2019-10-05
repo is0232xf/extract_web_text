80fd96048fd8f3600d44d75dc7645bef692b51cc
Yasuyuki Fujii <is0232xf@ed.ritsumei.ac.jp>
2019-10-05 23:39:32 +0900

    make a frame code

--- extract_web_text.py
+++ extract_web_text.py
@@ -5,3 +5,11 @@ Created on Sat Oct  5 23:31:43 2019
 @author: Yasuyuki
 """
 
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://ejje.weblio.jp/content/extract"
+
+r = requests.get(url)
+soup = BeautifulSoup(r.content, "html.parser")
+print(soup.find("div", id="summary").text)
