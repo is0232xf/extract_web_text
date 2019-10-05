bd54bccb74f89832a6a75dd77128b803f38941f8
Yasuyuki Fujii <is0232xf@ed.ritsumei.ac.jp>
2019-10-06 00:20:05 +0900

    [add] search multiple words on weblio

--- extract_web_text.py
+++ extract_web_text.py
@@ -7,13 +7,14 @@ Created on Sat Oct  5 23:31:43 2019
 import requests
 from bs4 import BeautifulSoup
 
-word = "extract"
-url = "https://ejje.weblio.jp/content/" + word
+search_word = ["extract", "word", "English", "tablet", "water", "play", "buoyancy"]
 
-r = requests.get(url)
-soup = BeautifulSoup(r.content, "html.parser")
-mean_text = soup.find("div", class_="summaryM descriptionWrp").text
-level = soup.find("span", class_="learning-level-content").text
-#text = text.replace("\n", "")
-print("mean: ", mean_text)
-print("lebel: ", level)
+for word in search_word:
+    url = "https://ejje.weblio.jp/content/" + word
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    mean_text = soup.find("div", class_="summaryM descriptionWrp").text
+    level = soup.find("span", class_="learning-level-content").text
+    #text = text.replace("\n", "")
+    print("mean: ", mean_text)
+    print("lebel: ", level)
