c2b2cb44aa26df6e8a3d31187109da7db8704432
Yasuyuki Fujii <is0232xf@ed.ritsumei.ac.jp>
2019-10-06 00:31:47 +0900

    [add] add comments

--- extract_web_text.py
+++ extract_web_text.py
@@ -7,15 +7,22 @@ Created on Sat Oct  5 23:31:43 2019
 import requests
 from bs4 import BeautifulSoup
 
+# make search words list
 search_words = ["extract", "word", "English", "tablet", "water", "play", "buoyancy"]
 
 for word in search_words:
+    # make url of each search word
     url = "https://ejje.weblio.jp/content/" + word
+
+    # send request to url
     r = requests.get(url)
+
+    # extract texts from web site
     soup = BeautifulSoup(r.content, "html.parser")
     mean_text = soup.find("div", class_="summaryM descriptionWrp").text
     level = soup.find("span", class_="learning-level-content").text
-    #text = text.replace("\n", "")
+
+    # print results
     print("--------------------------")
     print("word: ", word)
     print("mean: ", mean_text)
