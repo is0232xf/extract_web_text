bac4ebae035e9b184cbb3fe433b1278fd8697e3c
Yasuyuki Fujii <is0232xf@ed.ritsumei.ac.jp>
2019-10-06 21:09:26 +0900

    [add] add comment

--- search_word_mean_on_web.py
+++ search_word_mean_on_web.py
@@ -22,12 +22,15 @@ def search_on_weblio(search_words):
     
         # extract texts from web site
         soup = BeautifulSoup(r.content, "html.parser")
+        
+        # if there is no word mean on weblio, the mean of word is assigned None
         mean_text = soup.find("div", class_="summaryM descriptionWrp")
         if mean_text is None:
             mean_text = "None"
         else:
             mean_text = mean_text.text
         
+        # if there is no word levcel on weblio, the level of word is assigned level 99
         level = (soup.find("span", class_="learning-level-content"))
         if level is None:
             level = 99
